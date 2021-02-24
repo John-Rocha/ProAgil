64620510d3d04d9cd3f353f34e4b9a8418160751
John-Rocha <52468127+John-Rocha@users.noreply.github.com>
2021-02-23 23:40:00 -0300

    Add componet users

---
+++ pa-frontend/src/app/users/users.component.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { error } from '@angular/compiler/src/util';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss'],
+})
+export class UsersComponent implements OnInit {
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    // tslint:disable-next-line: deprecation
+    this.users = this.http.get('http://localhost:5000/api/values').subscribe(
+      (resp) => {
+        this.users = resp;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+}
